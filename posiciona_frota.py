59baf472b99d2238b31ea94cf9b11a49bd099f73
Maria <mariacnm2@al.insper.edu.br>
2023-01-23 20:05:14 -0300

    posicona- frota

---
+++ posiciona_frota.py
@@ -0,0 +1,48 @@
+def posiciona_frota(frota):
+    tabuleiro=[
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
+
+    for pos in frota.values():
+        for l in pos:
+            for listaazul in l:
+                x=listaazul[0]
+                y= listaazul[1]
+                tabuleiro[x][y]=1
+    return tabuleiro
+
+
+
+
+
+
+frota = {
+    "porta-aviões":[[[1,5],[1,6],[1,7],[1,8]]],
+    "navio-tanque":[
+      [[6,1],[6,2],[6,3]],
+      [[4,7],[5,7],[6,7]]
+    ],
+    "contratorpedeiro":[
+      [[1,1],[2,1]],
+      [[2,3],[3,3]],
+      [[9,1],[9,2]]
+    ],
+    "submarino": [
+      [[0,3]],
+      [[4,5]],
+      [[8,9]],
+      [[8,4]]
+    ],
+}
+resultado = posiciona_frota(frota)
+print(resultado)
