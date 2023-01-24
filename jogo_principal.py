b3c557c54456ea3f8071b4c48cb30339b486e5c1
Maria <mariacnm2@al.insper.edu.br>
2023-01-24 17:18:57 -0300

    jogo_principal em andamento

--- jogo_principal.py
+++ jogo_principal.py
@@ -88,7 +88,9 @@ def posicao_valida(frota, linha, coluna, orientacao, tamanho):
 
 cont1=True
 cont2 = True
-input("Insira as informações referentes ao navio porta-aviões que possui tamanho 4")
+cont3=True
+cont4=True
+print("Insira as informações referentes ao navio porta-aviões que possui tamanho 4")
 
 while cont1:
     porta_aviões_linha = int (input('Linha:'))
@@ -104,13 +106,13 @@ while cont1:
     else:
         valida_pa== False
         print("Esta posição não está válida!")
-define= (define_posicoes(porta_aviões_linha,porta_aviões_coluna,porta_aviões_orientação,4))
-frota=(preenche_frota({},'porta-aviões',porta_aviões_linha,porta_aviões_coluna,porta_aviões_orientação,4))
+#define1= (define_posicoes(porta_aviões_linha,porta_aviões_coluna,porta_aviões_orientação,4))
+frota1=(preenche_frota({},'porta-aviões',porta_aviões_linha,porta_aviões_coluna,porta_aviões_orientação,4))
 
+soma1=0
 i=1
-
 while cont2:
-    input("Insira as informações referentes ao navio navio-tanque que possui tamanho 3")
+    print("Insira as informações referentes ao navio navio-tanque que possui tamanho 3")
     navio_tanque_linha = int (input('Linha:'))
     navio_tanque_coluna = int (input('Coluna:'))
     navio_tanque_orientacao = int (input ('Orientação'))
@@ -120,13 +122,56 @@ while cont2:
         navio_tanque_orientacao = 'horizontal'
     valida_pa=posicao_valida({},navio_tanque_linha,navio_tanque_coluna,navio_tanque_orientacao,3)
     if valida_pa== False:
+        soma1+=1
         print("Esta posição não está válida!")
+        if i>soma1:
+            i-=1
     if i>=2:
         cont2=False
     i+=1
+#define2= (define_posicoes(navio_tanque_linha,navio_tanque_coluna,navio_tanque_orientacao,3))
+frota2=(preenche_frota(frota1,'navio-tanque',navio_tanque_linha,navio_tanque_coluna,navio_tanque_orientacao,3))
 
-
-
+j=1
+soma2=0
+while cont3:
+    print("Insira as informações referentes ao navio contratorpedeiro que possui tamanho 2")
+    navio_contratorpedeiro_linha = int (input('Linha:'))
+    navio_contratorpedeiro_coluna = int (input('Coluna:'))
+    navio_contratorpedeiro_orientacao = int (input ('Orientação'))
+    if navio_contratorpedeiro_orientacao == 1 :
+        navio_contratorpedeiro_orientacao='vertical'
+    if navio_contratorpedeiro_orientacao == 2:
+        navio_contratorpedeiro_orientacao = 'horizontal'
+    valida_pa=posicao_valida({},navio_contratorpedeiro_linha,navio_contratorpedeiro_coluna,navio_contratorpedeiro_orientacao,2)
+    if valida_pa== False:
+        soma2+=1
+        print("Esta posição não está válida!")
+        if j>soma2:
+            j-=1
+    if j>=3:
+        cont3=False
+    j+=1
+#define3= (define_posicoes(navio_contratorpedeiro_linha,navio_contratorpedeiro_coluna,navio_contratorpedeiro_orientacao,2))
+frota3=(preenche_frota(frota2,'contratorpedeiro',navio_contratorpedeiro_linha,navio_contratorpedeiro_coluna,navio_contratorpedeiro_orientacao,2))
+m=1
+soma3=0
+while cont4:
+    print("Insira as informações referentes ao navio submarino que possui tamanho 1")
+    submarino_linha = int (input('Linha:'))
+    submarino_coluna = int (input('Coluna:'))
+    valida_pa=posicao_valida({},submarino_linha,submarino_coluna,'horizontal',1)
+    if valida_pa== False:
+        soma3+=1
+        print("Esta posição não está válida!")
+        if i>soma3:
+            i-=1
+    if m>=4:
+        cont4=False
+    m+=1
+#define4= (define_posicoes(navio_contratorpedeiro_linha,navio_contratorpedeiro_coluna,navio_contratorpedeiro_orientacao,2))
+frota4=(preenche_frota(frota3,'contratorpedeiro',submarino_linha,submarino_coluna,'horizontal',1))
+print(frota4)
 
 
 
