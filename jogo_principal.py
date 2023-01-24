6567514f5bff3d4a96b150650a4979fdcc6a4c0b
Maria <mariacnm2@al.insper.edu.br>
2023-01-24 14:58:48 -0300

    começando jogo

---
+++ jogo_principal.py
@@ -0,0 +1,87 @@
+def define_posicoes(linha,coluna,orientacao,tamanho):
+    posicao=[0]*tamanho
+    
+    if orientacao== 'vertical':
+        for i in range(len(posicao)):
+            pos=[]
+            pos.append(linha)
+            pos.append(coluna)
+            linha+=1
+            posicao[i]=pos
+        return posicao
+    else:
+        for i in range(len(posicao)):
+            pos=[]
+            pos.append(linha)
+            pos.append(coluna)
+            coluna+=1
+            posicao[i]=pos
+        return posicao
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    define=(define_posicoes(linha,coluna,orientacao,tamanho))
+    if nome_navio not in frota:
+        frota[nome_navio]=[define]
+    else:
+        frota[nome_navio].append(define)
+
+    return frota
+def faz_jogada(tabuleiro, linha, coluna):
+    if tabuleiro[linha][coluna] ==1:
+        tabuleiro[linha][coluna] = 'X'
+        return tabuleiro
+    else:
+        tabuleiro[linha][coluna] = '-'
+        return tabuleiro
+def posiciona_frota(frota):
+    tabuleiro=[
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
+
+    for pos in frota.values():
+        for l in pos:
+            for listaazul in l:
+                x=listaazul[0]
+                y= listaazul[1]
+                tabuleiro[x][y]=1
+    return tabuleiro
+def afundados(frota,tabuleiro):
+    soma=0
+    for posicoes in frota.values():
+        for linha in posicoes:
+            ajuda=0
+            for numeros in linha:
+                y=numeros[0]
+                x=numeros[1]
+                if tabuleiro[y][x] == "X":
+                    ajuda+=1
+            if ajuda == len(linha):
+                soma+=1
+    return soma
+def posicao_valida(frota, linha, coluna, orientacao, tamanho):
+    define=define_posicoes(linha,coluna,orientacao,tamanho)
+    
+    for listinha in define:
+        if listinha[0]>9 or listinha[1]>9:
+            return False
+
+
+    for posicoes in frota.values():
+        for linha in posicoes:
+            for par in linha:
+                for par2 in define:
+                    if par[0] == par2[0] and par[1] == par2[1]:
+                        return False
+    return True
+
+
+
